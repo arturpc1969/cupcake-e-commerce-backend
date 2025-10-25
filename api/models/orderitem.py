918c33d1ecce0ec3e8a63e997f8d8c215a2b4972
Artur Coutinho <arturpc@yahoo.com.br>
2025-10-25 18:08:35 -0300

    PIT-29 - Add constraint to ensure that a product is registered only once in an order

--- api/models/orderitem.py
+++ api/models/orderitem.py
@@ -9,5 +9,10 @@ class OrderItem(models.Model):
     quantity = models.PositiveIntegerField(default=1)
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['order', 'product'], name='unique_product_per_order')
+        ]
+
     def __str__(self):
-        return f"{self.quantity}x {self.product.name} (Order {self.order.order_number})"
+        return f"{self.quantity} x {self.product.name} (Order {self.order.order_number})"
